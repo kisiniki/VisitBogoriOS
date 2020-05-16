77b850dd13eb636589c9c05bc30b7ef19b371c33
Ishaq Fikri Taufik <ishaq.14117174@student.itera.ac.id>
2020-05-17 02:26:17 +0700

    Initial Commit

---
+++ VisitBogor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisitBogor
+//
+//  Created by Ishaq Fikri Taufik on 17/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
